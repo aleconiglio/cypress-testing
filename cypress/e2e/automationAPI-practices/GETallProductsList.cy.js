b0d0db6be90ac3b8d82c481b283421e4ee6aff96
aleconiglio <coniglio.alejo@gmail.com>
2023-03-16 20:12:11 -0300

    api testing

---
+++ cypress/e2e/automationAPI-practices/GETallProductsList.cy.js
@@ -0,0 +1,13 @@
+describe('get all products list', () => {
+    it('GET 200', () => {
+        cy.request({
+            method: 'GET',
+            url: "https://automationexercise.com/api/productsList"
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            cy.step("products: ")
+            cy.log(JSON.stringify(response));
+        });
+        
+    });
+});
